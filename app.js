58e80c6992735bf60ec791356befafe23d6738e3
Tatsuya Kawasaki <ta2ya.xxx@gmail.com>
2011-06-24 01:53:06 +0900

    oauth connect success

--- app.js
+++ app.js
@@ -1,21 +1,45 @@
+require.paths.unshift('./node_modules');
 
 /**
  * Module dependencies.
  */
+var express = require('express')
+    everyauth = require('everyauth'),
+    git_config = require(__dirname + "/config/github");
 
-var express = require('express');
+/**
+ * OAuth Setting
+ */
+everyauth
+  .github
+    .myHostname('http://geek_meets_designer.cloudfoundry.com')
+    .appId(git_config.appId)
+    .appSecret(git_config.appSecret)
+    .entryPath('/auth/github')
+    .callbackPath('/auth/github/callback')
+    .scope('public_repo')
+    .findOrCreateUser( function (session, accessToken, accessTokenExtra, githubUserMetadata) {
+      // find or create user logic goes here
+      return {name:githubUserMetadata.user}
+    })
+    .redirectPath('/auth/github/loginhook');
 
-var app = module.exports = express.createServer();
+var app = module.exports = express.createServer(
+    express.bodyParser()
+  , express.methodOverride()
+  , express.static(__dirname + "/public")
+  , express.cookieParser()
+  , express.session({ secret: 'htuayreve'})
+  , everyauth.middleware()
+);
+everyauth.helpExpress(app);
 
 // Configuration
 
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
   app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
@@ -26,13 +50,23 @@ app.configure('production', function(){
   app.use(express.errorHandler()); 
 });
 
-// Routes
 
+// Routes
 app.get('/', function(req, res){
   res.render('index', {
-    title: 'Express'
+    title: 'Express' 
   });
 });
 
-app.listen(3000);
-console.log("Express server listening on port %d", app.address().port);
+app.get('/auth/github/loginhook', function(req, res){
+  console.log(req.loggedIn);
+  console.log(req.user);
+  res.redirect('/');
+});
+
+
+var host = process.env.VCAP_APP_HOST || 'localhost';
+var port = process.env.VMC_APP_PORT || 3000;
+
+app.listen(port, host);
+console.log('application run !! http://' + host + ':' + port );
