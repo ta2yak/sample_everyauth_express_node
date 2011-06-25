94d1341bac3826817dc9ae39259d21470e1cf4ad
Tatsuya Kawasaki <ta2ya.xxx@gmail.com>
2011-06-25 10:00:04 +0900

    first commit on heroku version

--- config/github.js
+++ config/github.js
@@ -1,7 +1,7 @@
 module.exports = {
-    host: 'http://geekmeetsdesigner.herokuapp.com'
-  , appId: 'bbd01e6b0915ccb59300'
-  , appSecret: '7e860218ebedc44cb6bb16e4cd2f2cd14109af55'
-  , entryPath: '/auth/github'
-  , callbackPath: '/auth/github/callback'
+    host: ''
+  , appId: ''
+  , appSecret: ''
+  , entryPath: ''
+  , callbackPath: ''
 };
